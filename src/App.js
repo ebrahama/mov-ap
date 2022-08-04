51277f82d31f7587ef9ea9ab42f378d5ca8c10aa
ibrahama <ibrahama2003@gmail.com>
2022-08-04 20:37:33 +0200

    sec fli ad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import axios from 'axios';
 import './App.css';
+import MovieCard from "./compon/MovieCard";
+
 
 function App() {
+
+const api = "https://api.themoviedb.org/3/discover/movie?api_key=47009c10eddb127f105f8aba2ad2688c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
+const [movies, setmovies] = useState( [])
+
+const fetmov = async () => {
+  const {data: {results}} = await axios.get(`${api}`, {}) 
+  setmovies(results)
+}
+
+
+
+useEffect(() => {
+  fetmov()
+}, [])
+
+
+const rendermovie = () => (
+
+   movies.map(movie => (
+       <MovieCard
+       key={movie.id}
+       movie={movie}
+       />
+   ))
+)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h2>hol</h2>
+
+      <div className="conta">
+      { rendermovie() }
+      </div>
+
+
     </div>
   );
+
+
+
 }
 
 export default App;
---
+++ src/compon/MovieCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MovieCard = ({movie}) =>{
+    return(
+        <div>
+         {movie.title}
+        </div>
+    );
+
+};
+
+export default MovieCard;
