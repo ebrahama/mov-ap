d07d9f5a27a3b2c7f2dbb2cbb2dcbc1acc4c0376
ibrahama <ibrahama2003@gmail.com>
2022-08-07 22:38:27 +0200

    ad-cs

--- src/App.js
+++ src/App.js
@@ -8,29 +8,24 @@ import YouTube from "react-youtube";
 function App() {
 
 const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280"
-
 const url = "https://api.themoviedb.org/3"
-//const api = "?api_key=47009c10eddb127f105f8aba2ad2688c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
 
 const [movies, setmovies] = useState( [])
 const [selectmov, setselectmov] = useState( {})
 const [searchkey, setsearchkey] = useState( "")
 const [playtr, setplaytr] = useState(false)
 
-
-
 const fetmov = async (searchkey) => {
 const types = searchkey ? "/search" : "/discover"
-//const types = "/movie/upcoming" ${api}
+//const types = "/movie/upcoming"
  const {data: {results}} = await axios.get(`${url}${types}/movie`, {
            params:{
             api_key: process.env.REACT_APP_MOVE_API,
             query: searchkey
            }
- }) 
-   selfmo(results[0])
- //setselectmov(results[0])
- setmovies(results)
+ })
+  selfmo(results[0])
+  setmovies(results)
 }
 
 const fmo = async (id) => {
@@ -43,18 +38,16 @@ const fmo = async (id) => {
   return data
   }
 
+
   const selfmo = async (movie) => {
     setplaytr(false)
     const data = await fmo(movie.id)
     setselectmov(data)
    }
 
-
-
 useEffect(() => {
   fetmov()
 }, [])
-
 
 const rendermovie = () => (
    movies.map(movie => (
@@ -72,16 +65,14 @@ const searchmovie = (e) => {
  }
 
 const Rendtr = () =>{
-
   const tra = selectmov.videos.results.find(vid => vid.name === 'Official Trailer')
   const tr = tra ? tra.key : selectmov.videos.results[0].key
-
   return (
     <YouTube
+    className="ytb"
     videoId={tr}
     />
        )
- 
   }
 
 
@@ -89,39 +80,47 @@ const Rendtr = () =>{
     <div className="App">
 
     <header>
-       <span>movies</span>
        <form onSubmit={searchmovie}>
-
        <input type="text" onChange={(e) =>{setsearchkey(e.target.value)}}/>
-     
        <button type={"submit"}>search</button>
-
        </form>
-       
     </header>
 
-    <div className="hero" style={{backgroundImage:`url('${IMAGE_PATH}${selectmov.backdrop_path}')`}}>
+    <div> 
+      <img className="hero" style={{backgroundImage:`url('${IMAGE_PATH}${selectmov.backdrop_path}')`}}></img>
       <div className="herocon">
-
             {selectmov.videos && playtr ? Rendtr() : null }
-
-             <button onClick={() => setplaytr(true)}>play trailer</button>
-             <button>watch now</button>
-
-              <h1>{selectmov.title}</h1>
-              {selectmov.overview ? <p>{selectmov.overview}</p> : null}
-      </div>
+              <h1 className="ti">{selectmov.title}</h1>
+              {selectmov.overview ? <p className="ov">{selectmov.overview}</p> : null}
+           <div className="jj">
+             <p>{selectmov.release_date}</p>  
+             <p>min: {selectmov.runtime}</p>
+           </div>
+            
+             <button className="wn">watch now</button>
+             <button className="tr" onClick={() => setplaytr(true)}>trailer</button> 
+        
+     <div className="moviecard3">
+      {selectmov.poster_path ? <img className="moviepost3" src={`${IMAGE_PATH}${selectmov.poster_path}`}></img>
+      :<div className="noimg3"></div>
+      } 
     </div>
 
 
-      <div className="conta">
+
+      </div>
+    </div>
+ 
+    <div className="cal">
+      <h2>Popular</h2>
+      <div className="conta"> 
       { rendermovie() }
       </div>
+    </div>
+    
 
     </div>
   );
-
-
 
 }
 
--- src/compon/MovieCard.js
+++ src/compon/MovieCard.js
@@ -6,13 +6,12 @@ const MovieCard = ({movie ,selectmov}) =>{
     const IMAGE_PATH = "https://image.tmdb.org/t/p/w300"
 
     return(
-        <div className="moviecard" onClick={() =>{ selectmov(movie)}}>
+        <div className="moviecard " onClick={() =>{selectmov(movie)}}>
 
          { movie.poster_path ? <img className="moviepost" src={`${IMAGE_PATH}${movie.poster_path}`}></img>
-         :<div className="noimg">@</div>
+         :<div className="noimg"></div>
         } 
             <h5 className="movietitle">{movie.title}</h5>
-
         </div>
     );
 
